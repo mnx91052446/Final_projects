564461a841b091030a3afa885990b291d5e98d42
mnx91052446 <44564398+mnx91052446@users.noreply.github.com>
2020-04-18 13:49:24 +0800

    add plan location
    Signed-off-by: mnx91052446 <44564398+mnx91052446@users.noreply.github.com>

--- app/src/main/java/com/example/final_projects/Plan.java
+++ app/src/main/java/com/example/final_projects/Plan.java
@@ -1,14 +1,219 @@
 package com.example.final_projects;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Plan extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plan extends FragmentActivity implements OnMapReadyCallback,View.OnClickListener {
+
+    private GoogleMap mMap;
+    private SearchView searchView;
+    private TextView textView;
+    private TextView textView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        searchView = findViewById(R.id.search);
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        findViewById(R.id.button5).setOnClickListener(this);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressesList = null;
+
+                if (location != null || !location.equals("")) {
+                    Geocoder geocoder = new Geocoder(Plan.this);
+                    try {
+                        addressesList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressesList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        mapFragment.getMapAsync(this);
+
+
     }
+
+
+    private boolean chkPlayService() {
+        int avai = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(Plan.this);
+        if (avai == ConnectionResult.SUCCESS) {
+            Log.i("Map Test", "Fine");
+            return true;
+        } else {
+            Toast.makeText(this, "版本不符合", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }//
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(24.070675, 120.714900);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("here"));
+
+
+
+
+    }
+
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button5:
+                requestUserLocation();
+                break;
+
+        }
+    }
+
+    private void requestUserLocation() {
+        final LocationManager mLocation = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //判斷當前是否已經獲得了定位權限
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(Plan.this, "權限沒開", Toast.LENGTH_SHORT).show();
+            requestCameraPermission();
+            return;
+        }
+        mLocation.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, new LocationListener() {
+
+
+            @Override
+            public void onLocationChanged(final Location location) {
+                final StringBuffer sb = new StringBuffer();
+                sb.append("Now location is : \n")
+                        .append("lat : " + location.getLatitude()).append("\n")
+                        .append("lng : " + location.getLongitude());
+                textView.setText("lat : " + location.getLatitude());
+                textView2.setText("lng : " + location.getLongitude());
+                Log.i("test", String.valueOf(location.getLatitude()));
+                onMapReady(mMap);
+                LatLng sydney = new LatLng(location.getLatitude(),location.getLongitude() );
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+
+
+                Toast.makeText(Plan.this, sb.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        }, Plan.this.getMainLooper());
+
+    }
+
+
+    private void requestCameraPermission() {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M)
+            return;
+
+        final List<String> permissionsList = new ArrayList<>();
+        if (this.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            permissionsList.add(Manifest.permission.CAMERA);
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            permissionsList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionsList.size() < 1)
+            return;
+        if (this.shouldShowRequestPermissionRationale(Manifest.permission.CAMERA))
+            this.requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), 0x00);
+        else
+            goToAppSetting();
+    }//方法一
+
+    private void goToAppSetting() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", this.getPackageName(), null));
+        startActivityForResult(intent, 0x00);
+    }//方法一
 }
+
